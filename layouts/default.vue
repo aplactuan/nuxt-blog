6b2a86781e2ce4288744c0b01a641d21ae0ac89c
Adrian Lactuan <aplactuan@gmail.com>
2022-02-25 18:06:14 +0800

    Add backgroung image to intro section, add header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,35 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="toggleSideNav" />
+    <TheSidenav
+      :show="displaySideNav"
+      @close="displaySideNav = false"
+    />
     <Nuxt />
   </div>
 </template>
+
+<script>
+  import TheHeader from '@/components/Navigation/TheHeader.vue';
+  import TheSidenav from "@/components/Navigation/TheSidenav.vue";
+
+  export default {
+    components: {
+      TheHeader,
+      TheSidenav
+    },
+    data() {
+      return {
+        displaySideNav: false
+      }
+    },
+    methods: {
+      toggleSideNav() {
+        this.displaySideNav = !this.displaySideNav;
+      }
+    }
+  }
+</script>
 
 <style>
   html {
