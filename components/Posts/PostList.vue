73e0e1b2f9c925760aca6380971e9111268ba812
Adrian Lactuan <aplactuan@gmail.com>
2022-03-04 22:33:13 +0800

    Preparing for the backend section, add edit post form and add post form

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,16 +1,19 @@
 <template>
   <section class="featured-posts">
     <post-preview id="1"
+                  :is-admin="isAdmin"
                   title="Post Title 1"
                   previewText="Preview Content 1"
                   thumbnail="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2017/02/Photoshop-Replace-Background-Featured.jpg"
     />
     <post-preview id="2"
+                  :is-admin="isAdmin"
                   title="Post Title 2"
                   previewText="Preview Content 2"
                   thumbnail="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2017/02/Photoshop-Replace-Background-Featured.jpg"
     />
     <post-preview id="3"
+                  :is-admin="isAdmin"
                   title="Post Title 3"
                   previewText="Preview Content 3"
                   thumbnail="https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2017/02/Photoshop-Replace-Background-Featured.jpg"
@@ -24,6 +27,12 @@
   export default {
     components: {
       PostPreview
+    },
+    props: {
+      isAdmin: {
+        type: Boolean,
+        default: false,
+      }
     }
   }
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="post-page">
+    <section class="update-form">
+      <AdminPostForm :post="postToEdit" />
+    </section>
+  </div>
+</template>
+
+<script>
+  import AdminPostForm from "@/components/Admin/AdminPostForm.vue";
+
+  export default {
+    components: {
+      AdminPostForm
+    },
+    data() {
+      return {
+        postToEdit: {
+          author: 'Adrian Lactuan',
+          title: 'Title Post',
+          thumbnailLink: 'https://static1.makeuseofimages.com/wordpress/wp-content/uploads/2017/02/Photoshop-Replace-Background-Featured.jpg',
+          content: 'This is a very beautiful post'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .update-form {
+    width: 90%;
+    margin: 20px auto;
+  }
+  @media (min-width: 768px) {
+    .update-form {
+      width: 500px;
+    }
+  }
+</style>
